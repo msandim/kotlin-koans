7b5c8e9d29cc0e9f7e4650af11214556a5959d22
Miguel Sandim <migsandim@hotmail.com>
2018-08-08 18:03:30 +0100

    finished the building section

--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -22,7 +22,7 @@ fun todoTask39(): Nothing = TODO(
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(color = getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -33,8 +33,22 @@ fun renderProductTable(): String {
                     text("Popularity")
                 }
             }
+
             val products = getProducts()
-            todoTask39()
+
+            for((index, product) in products.withIndex()) {
+                tr {
+                    td(color = getCellColor(index, 1)) {
+                        text(product.description)
+                    }
+                    td(color = getCellColor(index, 2)) {
+                        text(product.price)
+                    }
+                    td(color = getCellColor(index, 3)) {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
